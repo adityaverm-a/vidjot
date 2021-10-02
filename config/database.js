6ec11cdbe4a2512a4e23ff97a8af9dee15730ced
adityaverm-a <aditya.se.403@gmail.com>
2021-10-02 21:32:44 +0530

    little fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://adi:adi@cluster0.jrh1g.mongodb.net/adi?retryWrites=true&w=majority'}
+  module.exports = {mongoURI: process.env.mongoURI}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
